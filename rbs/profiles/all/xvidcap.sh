bd94f20a6280bf829d2a5172d7759177886a02d6
ralph.j.jones.iii <ralph.j.jones.iii@2f03cc20-792d-11de-bdc7-1f637afd43b1>
2009-10-04 20:56:19 +0000

    more fixes

--- rbs/profiles/all/xvidcap.sh
+++ rbs/profiles/all/xvidcap.sh
@@ -25,7 +25,8 @@ build(){
   unpack_tarball $TARBALL || return 1
   cd $SRCDIR/$DIR || return 1
   
-  do_patch xvidcap-1.1.7-ffmpeg-20090601x-1.patch || return 1
+  do_patch xvidcap-1.1.7-ffmpeg-20090601x-1.patch \
+    xvidcap-1.1.7-xextproto71.patch || return 1
   
   CC="$CC $BUILD" CXX="$CXX $BUILD" ./configure --prefix=/usr \
     --libdir=/usr/$LIBSDIR --sysconfdir=/etc || return 1
