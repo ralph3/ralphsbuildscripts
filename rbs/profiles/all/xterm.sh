88ac61c940746e9a36f9523eadc2e8f0e61215cd
ralph.j.jones.iii <ralph.j.jones.iii@2f03cc20-792d-11de-bdc7-1f637afd43b1>
2010-03-30 18:00:14 +0000

    update

---
+++ rbs/profiles/all/xterm.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DISABLE_MULTILIB=1
+
+VERSION="243"
+
+DIR="xterm-${VERSION}"
+TARBALL="xterm-${VERSION}.tgz"
+
+DEPENDS=(
+  xorg-server
+)
+
+SRC1=(
+ftp://invisible-island.net/xterm/${TARBALL}
+)
+
+MD5SUMS=(
+be25d2d3661d0bb77fc66e1bcbb3caba
+)
+
+build(){
+  unpack_tarball $TARBALL || return 1
+  cd $SRCDIR/$DIR || return 1
+  CC="$CC $BUILD" CXX="$CXX $BUILD" ./configure --prefix=/usr \
+    --libdir=/usr/$LIBSDIR --sysconfdir=/etc --localstatedir=/var || return 1
+  make || return 1
+  make install DESTDIR=$TMPROOT || return 1
+  cd ../ || return 1
+  rm -rf $DIR || return 1
+}
+
+version_check_info(){
+  ADDRESS='ftp://invisible-island.net/xterm/'
+  VERSION_STRING='xterm-%version%.tgz'
+  MIRRORS=(
+    'ftp://invisible-island.net/xterm/xterm-%version%.tgz'
+  )
+}
