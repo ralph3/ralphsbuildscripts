0adfec9f98c41eae524e6f6ae57ce403df5b6c8e
ralph.j.jones.iii <ralph.j.jones.iii@2f03cc20-792d-11de-bdc7-1f637afd43b1>
2010-04-11 11:24:08 +0000

    up

--- rbs/profiles/all/cloog-ppl.sh
+++ rbs/profiles/all/cloog-ppl.sh
@@ -20,9 +20,11 @@ MD5SUMS=(
 RBS_Cross_Tools_Build(){
   unpack_tarball $TARBALL || return 1
   cd $SRCDIR/$DIR || return 1
+  sed -i "/LD_LIBRARY_PATH=/d" configure || return 1
   LDFLAGS="-Wl,-rpath,/RBS-Cross-Tools/${LIBSDIR}" \
     ./configure --prefix=/RBS-Cross-Tools --enable-shared \
-    --with-gmp=/RBS-Cross-Tools --with-ppl=/RBS-Cross-Tools || return 1
+    --with-bits=gmp --with-gmp=/RBS-Cross-Tools \
+    --with-ppl=/RBS-Cross-Tools || return 1
   make || return 1
   make install || return 1
   cd ../ || return 1
@@ -32,10 +34,12 @@ RBS_Cross_Tools_Build(){
 RBS_Tools_Build(){
   unpack_tarball $TARBALL || return 1
   cd $SRCDIR/$DIR || return 1
+  sed -i "/LD_LIBRARY_PATH=/d" configure || return 1
   CC="$CC $BUILD" CXX="$CXX $BUILD" LDFLAGS="-lm -lstdc++" \
-    ./configure --build=$BUILDHOST \
-    --host=$BUILDTARGET --prefix=/RBS-Tools --libdir=/RBS-Tools/$LIBSDIR \
-    --enable-shared --with-gmp=/RBS-Tools --with-ppl=/RBS-Tools || return 1
+    ./configure --build=$BUILDHOST --host=$BUILDTARGET \
+    --prefix=/RBS-Tools --libdir=/RBS-Tools/$LIBSDIR \
+    --enable-shared --with-gmp=/RBS-Tools --with-ppl=/RBS-Tools \
+    --with-bits=gmp || return 1
   make || return 1
   make install || return 1
   cd ../ || return 1
