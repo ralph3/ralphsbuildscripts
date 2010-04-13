1ab437d44c029f4ee2d5a2763f67beb6c3ecf9e5
ralph.j.jones.iii <ralph.j.jones.iii@2f03cc20-792d-11de-bdc7-1f637afd43b1>
2010-04-13 13:29:27 +0000

    update

--- rbs/profiles/all/mesa.sh
+++ rbs/profiles/all/mesa.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VERSION="7.8.1"
+SYS_VERSION="7.8.1-2"
 
 DIR="Mesa-${VERSION}"
 
@@ -22,9 +23,14 @@ SRC2=(
 ftp://ftp.freedesktop.org/pub/mesa/${VERSION}/MesaGLUT-${VERSION}.tar.bz2
 )
 
+SRC3=(
+ftp://ftp.freedesktop.org/pub/mesa/${VERSION}/MesaDemos-${VERSION}.tar.bz2
+)
+
 MD5SUMS=(
 25ec15f8e41fde6d206118cc786dbac4
 6bae516a44c6d26ff3152c960ab648e7
+9ef47f911869657c6bf2f43ebce86b61
 )
 
 build(){
@@ -37,7 +43,8 @@ build(){
   
   mkdir -p $SRCDIR || return 1
   for x in MesaLib-${VERSION}.tar.bz2 \
-           MesaGLUT-${VERSION}.tar.bz2 ; do
+           MesaGLUT-${VERSION}.tar.bz2 \
+           MesaDemos-${VERSION}.tar.bz2; do
     echo -n "Unpacking ${x}..."
     tar xfj $DOWNLOADDIR/$x -C $SRCDIR || return 1
     echo " done"
@@ -49,6 +56,13 @@ build(){
   make || return 1
   make install DESTDIR=$TMPROOT || return 1
   
+  mkdir -vp $TMPROOT/usr/bin || return 1
+  install -m755 -v progs/xdemos/{glxinfo,glxgears} $TMPROOT/usr/bin/ || return 1
+  set_multiarch $TMPROOT/usr/bin/{glxinfo,glxgears} || return 1
+  
+  [ "$SYSTYPE" == "MULTILIB" ] && \
+    [ "$BUILD" == "$BUILD32" ] && rm -rf $TMPROOT/$LIBSDIR/dri || return 1
+  
   cd $SRCDIR || return 1
   rm -rf $DIR || return 1
 }
@@ -59,5 +73,6 @@ version_check_info(){
   MIRRORS=(
     'ftp://ftp.freedesktop.org/pub/mesa/%version%/MesaLib-%version%.tar.bz2'
     'ftp://ftp.freedesktop.org/pub/mesa/%version%/MesaGLUT-%version%.tar.bz2'
+    'ftp://ftp.freedesktop.org/pub/mesa/%version%/MesaDemos-%version%.tar.bz2'
   )
 }
